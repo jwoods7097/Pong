39c62607de85fdaf8965560e22c46127ab15cfc8
sboese1 <samboese3@gmail.com>
2021-12-01 20:08:31 -0600

    Added the ability to move paddles

--- src/Game.java
+++ src/Game.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.io.File;
 import java.io.IOException;
 
 public class Game extends JPanel implements KeyListener, ActionListener {
@@ -79,6 +78,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     // This method is called every time the game updates
     @Override
     public void actionPerformed(ActionEvent e) {
+        ball.x += 1;
         repaint(); // Leave me at the bottom!
     }
 
@@ -89,7 +89,6 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println("TEST");
         int key = e.getKeyCode();
 
         if (key == KeyEvent.VK_UP) {
